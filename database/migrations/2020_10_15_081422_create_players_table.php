ba6325bfdbee69b83b073145060e6e0a3a386f32
hyseanlin <hylin827@gmail.com>
2020-10-15 16:28:06 +0800

    在資料庫伺服器(MySQL)中，建立 players 資料表格，並建立關聯式表格：讓 players 表格的 tid 欄位關聯到 teams 表得的 id 欄位。

---
+++ database/migrations/2020_10_15_081422_create_players_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id()->comment('編號');
+            $table->string('name')->comment('姓名');
+            $table->foreignId('tid')->comment('球隊編號');
+            $table->string('position')->comment('位置');
+            $table->float('height')->comment('身高');
+            $table->float('weight')->comment('體重');
+            $table->tinyInteger('year')->comment('年資');
+            $table->string('nationality')->comment('國籍');
+            $table->foreign('tid')
+                ->references('id')
+                ->on('teams')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
