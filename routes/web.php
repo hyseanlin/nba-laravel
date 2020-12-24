9a242df9bdb0acbc739def8e0f837600e8f1ed51
hyseanlin <hylin827@gmail.com>
2020-12-24 14:28:25 +0800

    Hotfix: redirect the route '/' to the route 'players,'

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\PlayersController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('players');
 });
 
 /*------------------------------------------
